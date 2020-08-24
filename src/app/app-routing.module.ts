ad0022f232de2e9bd98fb2b1bebdc5a9dcc5d4fe
kevinfcc26 <kevinfcc26@gmail.com>
2020-08-24 17:52:56 -0500

    added bootstrap and create components for app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { CharactersComponent } from './components/characters/characters.component';
+import { StudentsComponent } from './components/students/students.component';
+import { TeachersComponent } from './components/teachers/teachers.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent},
+  { path: 'characters', component: CharactersComponent},
+  { path: 'students', component: StudentsComponent},
+  { path: 'teachers', component: TeachersComponent},
+  { path: '**', pathMatch: 'full', redirectTo: 'home'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { TableComponent } from './components/table/table.component';
+import { CharactersComponent } from './components/characters/characters.component';
+import { StudentsComponent } from './components/students/students.component';
+import { TeachersComponent } from './components/teachers/teachers.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    TableComponent,
+    CharactersComponent,
+    StudentsComponent,
+    TeachersComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
